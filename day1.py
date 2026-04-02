61d3b9c872236ce689b0cf8dd39226e276810e6b
naveenca1611 <naveenkuamrca1611@gmail.com>
2026-04-02 14:43:01 +0530

    Day 1 - Python basics and user input script

---
+++ day1.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+role = input("Enter your role: ")
+age = input("Enter your age: ")
+
+print("--- User Info ---")
+print("Name:", name)
+print("Role:", role)
+print("Age:", age)
+
+print("Welcome", name, "! You are starting your GenAI journey 🚀")
